06ede0192acbad8bbfad0bda49106e2e312838d4
br7nnoo <brunnoyokoyama1@gmail.com>
2026-04-11 08:39:16 -0300

    Calculo de IMC

--- aula.c
+++ aula.c
@@ -3,45 +3,27 @@
 #include <iso646.h>
 
 int main(){
-           float camisa_valor, valor_bruto, desconto, porcentagem_desconto, valor_final;
-           int quantidade_de_camisas;
+           float altura, peso, IMC;
 
+printf("qual seu peso?\n");
+scanf("%f", &peso);
+printf("qual sua altura?\n");
+scanf("%f", &altura);
 
-           printf("Preco da camisa?\n");
-           scanf("%f", &camisa_valor);
-           printf("Quantas camisetas deseja comprar?\n");
-           scanf("%d", &quantidade_de_camisas);
+IMC = peso / (altura * altura);
 
-            valor_bruto = camisa_valor * quantidade_de_camisas;
+if(IMC < 18.50){
+printf("seu IMC e de %.2f, voce esta abaixo do peso", IMC);
 
-            if(quantidade_de_camisas <= 2){
+}else if(IMC >= 18.50 and IMC <= 24.90){
+printf("seu IMC e de %.2f, seu peso esta normal", IMC);
 
-                printf("Ficou %.2f", valor_bruto);
-            }else if(quantidade_de_camisas >= 3 and quantidade_de_camisas <= 5){
-                            porcentagem_desconto = 0.10;
+}else if(IMC >= 25.00 and IMC <= 29.00){
+printf("seu IMC e de %.2f, voce esta com sobrepeso", IMC);
 
-                            desconto = valor_bruto * porcentagem_desconto;
-
-                            valor_final = valor_bruto - desconto;
-
-                printf("voce ganhou %.2f de desconto, economizou %.2f e o valor final sera de %.2f", porcentagem_desconto, desconto, valor_final);
-            }else if(quantidade_de_camisas >= 6 and quantidade_de_camisas >= 10){
-                            porcentagem_desconto = 0.15;
-
-                            desconto = valor_bruto * porcentagem_desconto;
-
-                            valor_final = valor_bruto - desconto;
-
-                printf("voce ganhou %.2f de desconto, economizou %.2f e o valor final sera de %.2f", porcentagem_desconto, desconto, valor_final);
-            }else{
-                            porcentagem_desconto = 0.20;
-
-                            desconto = valor_bruto * porcentagem_desconto;
-
-                            valor_final = valor_bruto - desconto;
-
-                printf("voce ganhou %.2f de desconto, economizou %.2f e o valor final sera de %.2f", porcentagem_desconto, desconto, valor_final);
-            }
+}else{
+printf("seu IMC e de %.2f, voce esta obeso", IMC);
+}
 
            
 
